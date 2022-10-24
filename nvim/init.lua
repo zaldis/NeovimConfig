4bbc0745827977f9e14dadf32fd83409d3a240db
Artur Kulik <arthur.kulik@gmail.com>
2022-10-24 13:39:20 +0300

    switch to lua for options

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,14 +4,14 @@ require('config/auto-cmp')
 require('config/lsp')
 require('config/telescope')
 
+local opt = vim.opt                                -- to set options
+local cmd = vim.cmd                                -- to execute Vim commands e.g. cmd('pwd')
 
-vim.cmd([[
-set number relativenumber
-set completeopt=menu,menuone,noselect
-set background=dark
-set shiftwidth=4
-set tabstop=4
-set expandtab
-colorscheme gruvbox
-]])
-
+opt.number = true                                  -- Show line numbers
+opt.relativenumber = true                          -- Relative line numbers
+opt.completeopt = {'menu', 'menuone', 'noselect'}  -- Completion options (for deoplete)
+opt.background = 'dark'
+opt.shiftwidth = 4                                 -- Size of an indent
+opt.tabstop = 4                                    -- Number of spaces tabs count for
+opt.expandtab = true                               -- Use spaces instead of tabs
+cmd 'colorscheme gruvbox'

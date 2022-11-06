70e83abe357d255562a577d9fa5417bcd00bbdbe
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-11-06 10:41:41 +0200

    Add Django config to DAP

--- nvim/lua/config/dap.lua
+++ nvim/lua/config/dap.lua
@@ -19,24 +19,34 @@ end;
 
 
 dap.adapters.python = {
-    type = 'executable';
-    command = get_python_path();
-    args = { '-m', 'debugpy.adapter' };
+    type = 'executable',
+    command = get_python_path(),
+    args = { '-m', 'debugpy.adapter' },
 }
+
+local getDjangoProgramFile = function ()
+    local folderName = vim.fn.input('Enter Django project folder> ')
+    return '${workspaceFolder}/' .. folderName .. '/manage.py'
+end
 
 dap.configurations.python = {
     {
-        -- The first three options are required by nvim-dap
-        type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-        request = 'launch';
-        name = "Launch file";
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch',
+        name = "Launch file",
 
         -- Options below are for debugpy 
         -- see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+        program = "${file}", -- This configuration will launch the current file if used.
+        pythonPath = get_python_path,
+    }, {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch Django',
 
-        program = "${file}"; -- This configuration will launch the current file if used.
-	    pythonPath = get_python_path;
-    },
+        program = getDjangoProgramFile,
+        args = {'runserver', '--noreload'}
+    }
 }
 
 
